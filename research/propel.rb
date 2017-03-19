c8bcc86ac0f331b1c08bedf4b52a5f6b4b4de119
Ben Crowell <notreal@fakewillnotwork.invalid>
2017-03-19 16:53:29 -0700

    fix bug in propel algorithm

--- research/propel.rb
+++ research/propel.rb
@@ -20,7 +20,9 @@ require_relative "#{$lib}/system"
 #   h -- in meters, sets the width of the window for averaging of paths
 #   g -- in meters, sets inverse strength of transverse force
 #   w -- 'hann' or 'square'
-#   dparmax -- unitless; ignore points on a path that differ in estimated parameter by more than this fractional amount
+#   dparmax -- unitless; prefer points on a path that differ in estimated parameter by less than this fractional amount;
+#              to turn off this feature, set dparmax=1.0; default=1.0; useful for out-and-back or lollipop routes
+#   prefer_forward -- try not to go back to an earlier index on a path; boolean 0,1; default=1
 #   eps -- distance to move in meters at each step
 #   skip -- e.g., if this is 10, then only write every 10th point to the output files
 #   bump -- boolean; if 1, then try to move past points with f=0
@@ -64,10 +66,11 @@ def main
   h = require_param(params,'h')
   g = require_param(params,'g')
   w = require_param(params,'w')
-  dparmax = require_param(params,'dparmax')
+  dparmax = optional_param(params,'dparmax',1.0)
   eps = require_param(params,'eps')
   skip = require_param(params,'skip')
   bump = optional_param(params,'bump',0)
+  prefer_forward = (optional_param(params,'prefer_forward',1)==1)
   $verbosity = optional_param(params,'verbosity',3)
   xi,yi,zi = spherical_to_cartesian(lati,loni,0.0,lat0,lon0) # we don't use zi
 
@@ -81,7 +84,7 @@ def main
 
   if $verbosity>=3 then print "bounding box: x=#{x_lo},#{x_hi}, y=#{y_lo},#{y_hi}, ngrid=#{ngrid}, lati,loni=#{lati},#{loni} xi,yi=#{xi},#{yi} l=#{l}\n" end
 
-  d,true_path = propel(paths,xi,yi,xi,yi,eps,h,g,w,l,dparmax,bump,box,ngrid,grid_d,grid)
+  d,true_path = propel(paths,xi,yi,xi,yi,eps,h,g,w,l,dparmax,bump,box,ngrid,grid_d,grid,prefer_forward)
         # true_path = array of points
         # FIXME -- assumes final point is same as initial
   print "total horizontal distance = #{d/1000.0} km\n"
@@ -127,26 +130,41 @@ def estimate_length(path)
   }
 end
 
-def propel(paths,xi,yi,xf,yf,eps,h,g,window_type,l,dparmax,bump,box,ngrid,grid_d,grid)
-  # l = estimated length of entire path
+def propel(paths,xi,yi,xf,yf,eps,h,g,window_type,l,dparmax,bump,box,ngrid,grid_d,grid,prefer_forward)
+  # l = estimated length of entire path, in meters
+  # dparmax = max fractional difference in estimated d (0 to 1)
   p = [xi,yi]
   n = paths.length
   d = 0.0 # integrated horizontal distance
   dpath = Array.new(n, 0.0) # ... for individual paths
+  warnings = Array.new(n, {})
   true_path = [p]
   max_par_diff = dparmax*l
   approaching_finish = false
   closest_approach_to_finish = 1.0e10
   i=0
-  most_recent_i = Array.new(paths.length, 0) # new, optional method to prevent backtracking and dithering
+  most_recent_i = Array.new(paths.length, 0)
   while true
     f = [0.0,0.0]
     0.upto(paths.length-1) { |m|
       path = paths[m]
       if p[0].nan? || p[1].nan? then print "p is nan in propel\n"; exit(-1) end
-      r,q,t,ii = closest_point(p,m,paths,box,ngrid,grid_d,grid,d,max_par_diff,most_recent_i[m])
+      closest = closest_point(p,m,paths,box,ngrid,grid_d,grid,d,max_par_diff,most_recent_i[m],prefer_forward,h*100.0)
+      r,q,t,ii = closest[0]
               # r,q,t=distance,point,tangent,index
-      if !ii.nil? then most_recent_i[m]=ii end
+      badness = closest[1] # normally 0
+      if badness>=2.0  && !(warnings[m].has_key?('badness')) then
+        warnings[m]['badness'] = true
+        print "track #{m} has badness score of #{badness}, #{r} meters away, at d=#{d}\n"
+      end
+      if r>1000.0 && !(warnings[m].has_key?('out_of_range')) then
+        warnings[m]['out_of_range'] = true
+        print "track #{m} is #{r} meters away, which is more than 1 km, at d=#{d}\n"
+      end
+      if false && m==0 then # qwe
+        print "track #{m} is #{r} meters away, i=#{ii}, at d=#{d}\n"
+      end
+      if !ii.nil? && ii>most_recent_i[m] then most_recent_i[m]=ii end
       next if r>=h
       u = r/h
       w = window(u,window_type)
@@ -156,8 +174,10 @@ def propel(paths,xi,yi,xf,yf,eps,h,g,window_type,l,dparmax,bump,box,ngrid,grid_d
       if f_lon[0].nan? || f_lon[1].nan? then print "f_lon is nan in propel\n"; exit(-1) end
       f = add2d(f,f_lon)
       # attractive transverse force
-      rhat = normalize2d(sub2d(q,p))
-      f_tr = scalar_mul2d(rhat,w*(r/g))
+      r = sub2d(q,p)
+      rmag = mag2d(r)
+      rhat = scalar_mul2d(r,1.0/rmag)
+      f_tr = scalar_mul2d(rhat,w*(rmag/g)) # should probably project out any comp. parallel to this segment
       if f_tr[0].nan? || f_tr[1].nan? then f_tr=[0.0,0.0] end # happens if r=0
       f = add2d(f,f_tr)
     }
@@ -167,7 +187,8 @@ def propel(paths,xi,yi,xf,yf,eps,h,g,window_type,l,dparmax,bump,box,ngrid,grid_d
         print "propel() terminated with f=0 at d=#{d} meters\n"
         break
       else
-        p = bump(p,paths,most_recent_i)
+        fail,p = do_bump(p,paths,most_recent_i,eps)
+        if fail then print "propel terminated, tried to bump past end\n"; break end
         next
       end
     end
@@ -196,7 +217,8 @@ def propel(paths,xi,yi,xf,yf,eps,h,g,window_type,l,dparmax,bump,box,ngrid,grid_d
       if bump!=1 then
         print "propel() terminated due to bouncing at d=#{d} meters\n"
       else
-        p = bump(p,paths,most_recent_i)
+        fail,p = do_bump(p,paths,most_recent_i,eps)
+        if fail then print "propel terminated, tried to bump past end\n"; break end
       end
     end
     i=i+1
@@ -205,19 +227,29 @@ def propel(paths,xi,yi,xf,yf,eps,h,g,window_type,l,dparmax,bump,box,ngrid,grid_d
 end
 
 # method to get past incorrect stops
-# bump each index by 1, and move path to new average
-def bump(p,paths,most_recent_i)
+# bump each index by 1, and move path to point on 0th path
+# (tried moving to average of the points, but the average could then be nowhere near any point)
+def do_bump(p,paths,most_recent_i,eps)
+  old_p = p
+
+  old_i = most_recent_i[0] # qwe
+
   0.upto(most_recent_i.length-1) { |m| most_recent_i[m] = most_recent_i[m]+1 }
-  p = [0.0,0.0]
-  0.upto(paths.length-1) { |m|
-    i = most_recent_i[m]
-    if paths[m][i].nil? then print "propel() terminated with f=0, unable to bump"; break end
-    x = paths[m][i]['p']
-    p = add2d(p,x)
-  }
-  p = scalar_mul2d(p,1.0/paths.length)
+
+  new_i = most_recent_i[0] # qwe
+  print "do_bump, old=#{old_i}, new=#{new_i}, length=#{paths[0].length}\n" # qwe
+
+  i = most_recent_i[0]
+  if i>paths[0].length-1 then return [true,p] end
+  p = paths[0][i]['p']
   p = add2d(p,[0.0001,0.0001]) # without this, we get errors because we're not in general position
-  return p
+  if dist2d(old_p,p)>10.0*eps then
+    print "warning in do_bump, hop of #{dist2d(old_p,p)}, #{paths[0][i-1]['p']}, #{paths[0][i]['p']}, bumped to i=#{most_recent_i[0]}\n"
+    0.upto(most_recent_i.length-1) { |m|
+      print "paths[#{m}][...]=#{paths[m][most_recent_i[m]]['p']}\n"
+    }
+  end
+  return [false,p]
 end
 
 def window(x,window_type)
@@ -250,17 +282,60 @@ def optional_param(params,name,default)
 end
 
 # Given a point x, find the nearest point (vertex or interior) on path m.
-# Return [distance,point,tangent].
-def closest_point(x,m,paths,box,ngrid,grid_d,grid,d,max_par_diff,not_before_i)
+# Return [[distance,point,tangent],badness].
+# This is a high-level routine, which actually calls a different workhorse routine to do the computations.
+# This routine's only job is to try to impose the preferences optionally defined by max_par_diff and
+# prefer_forward. It looks for solutions that lie closer than the distance defined by grab_if_closer_than,
+# and if it finds some of those, it returns the one that comes closest to satisfying the preferences.
+# If no points that close are found, it resorts to scoring the solutions on a heuristic scale (badness).
+# A normal result has badness=0. A badness of 2 is pretty bad, probably worth warning about.
+def closest_point(x,m,paths,box,ngrid,grid_d,grid,d,max_par_diff,not_before_i,prefer_forward,grab_if_closer_than)
+  candidates = []
+  i_par_diff_lo = (max_par_diff<1.0e10 ? 1 : 2)
+  i_force_forward_lo = (prefer_forward ? 1 : 2)
+  i_par_diff_lo.upto(2) { |i_par_diff|
+    i_force_forward_lo.upto(2) { |i_force_forward|
+      badness = 3.0*(i_par_diff-i_par_diff_lo)+1.0*(i_force_forward-i_force_forward_lo)
+      force_forward = (i_force_forward==1)
+      actual_max_par_diff = (i_par_diff==1 ? max_par_diff : 1.0e10)
+      candidate = closest_point_low_level(x,m,paths,box,ngrid,grid_d,grid,d,actual_max_par_diff,not_before_i,force_forward)
+      min_d,best_p,best_t,best_i = candidate
+      if min_d<grab_if_closer_than then return [candidate,badness] end
+      badness = badness+Math.log10(min_d/grab_if_closer_than)
+              # the log term is guaranteed to be >0
+      candidates.push([badness,candidate,i_par_diff,i_force_forward])
+    }
+  }
+  candidates.sort! { |a,b| a[0] <=> b[0] }
+  badness = candidates[0][0]
+  result = candidates[0][1]
+
+  min_d,best_p,best_t,best_i = result
+  if min_d>1.0e6 then
+    print "error, closest_point is #{min_d} meters away, prefer_forward=#{prefer_forward}, max_par_diff=#{max_par_diff}\n"
+    candidates.each { |a|
+      print "  min_d,best_p,best_t,best_i = #{a[1]}, badness=#{a[0]}, i_par_diff=#{a[2]}, i_force_forward=#{a[3]}\n"
+    }
+    exit(-1)
+  end
+  
+  return [result,badness]
+
+end
+
+# workhorse routine called by closest_point()
+# we call it up to four times for different combinations of max_par_diff and force_forward
+def closest_point_low_level(x,m,paths,box,ngrid,grid_d,grid,d,max_par_diff,not_before_i,force_forward)
   if x[0].nan? || x[1].nan? then print "x is nan in closest_point\n"; exit(-1) end
   i,j=point_to_grid(x,box,grid_d)
   min_d = 1.0e10
   best_p = nil
   best_t = nil
   best_i = nil
+  n_tried = 0
   0.upto(ngrid-1) { |r|
     # trace a 2r x 2r square around grid square i,j
-    -r.upto(r) { |u|
+    (-r).upto(r) { |u|
       best = (r-1.0)*grid_d # closest we could get from now on
       return [min_d,best_p,best_t,best_i] if best>min_d*1.414 # This is the normal exit, but there is another below.
       0.upto(3) { |edge| # top, bottom, right, left
@@ -275,7 +350,8 @@ def closest_point(x,m,paths,box,ngrid,grid_d,grid,d,max_par_diff,not_before_i)
           mm,v = seg # path mm's segment from vertex v to its successor may cross (ii,jj)
           next if mm!=m
           next if (v['d']-d).abs>max_par_diff
-          next if v['i']<not_before_i
+          n_tried = n_tried+1
+          next if force_forward && v['i']<not_before_i
           dd,p,t = closest_point_on_segment(x,v['p'],v['succ']['p'])
           if dd<min_d then
             min_d=dd
@@ -287,6 +363,11 @@ def closest_point(x,m,paths,box,ngrid,grid_d,grid,d,max_par_diff,not_before_i)
       }
     }
   }
+  if min_d>1.0e8 && max_par_diff>1.0e6 && !force_forward then
+    print "something is wrong in closest_point_low_level, min_d=#{min_d}, max_par_diff=#{max_par_diff}, force_forward=#{force_forward}\n"
+    print "  n_tried=#{n_tried}, d=#{d}\n"
+    exit(-1)
+  end
   return [min_d,best_p,best_t,best_i] # This is not the normal way we exit.
 end
 
